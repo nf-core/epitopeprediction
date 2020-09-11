1b97b0b051dfa26317646e0e6bd2e57f1a32ad5d
Christopher Mohr <christopher.mohr@uni-tuebingen.de>
2020-09-11 08:09:06 +0200

    Remove cast to bool

--- bin/epaa.py
+++ bin/epaa.py
@@ -825,7 +825,7 @@ def make_predictions_from_variants(variants_all, methods, alleles, minlength, ma
             if ('HLA-' in str(c)) or ('H-2-' in str(c)):
                 idx = df.columns.get_loc(c)
                 df.insert(idx + 1, '%s affinity' % c, df.apply(lambda x: create_affinity_values(str(c), int(x['length']), float(x[c]), x['Method'], max_values_matrices, allele_string_map), axis=1))
-                df.insert(idx + 2, '%s binder' % c, df.apply(lambda x: bool(create_binder_values(float(x['%s affinity' % c]), x['Method'])), axis=1))
+                df.insert(idx + 2, '%s binder' % c, df.apply(lambda x: create_binder_values(float(x['%s affinity' % c]), x['Method']), axis=1))
                 df = df.rename(columns={c: '%s score' % c})
                 df['%s score' % c] = df.apply(lambda x: create_score_values(float(x['%s score' % c]), x['Method']), axis=1)
 
