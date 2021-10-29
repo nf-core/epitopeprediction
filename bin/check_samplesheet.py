a8389f41608f07be402a88e17c36c0982257f969
jonas <jonas-scheid@live.de>
2021-10-29 14:25:45 +0200

    fixed review problems

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -8,7 +8,7 @@ import re
 
 
 def parse_args(args=None):
-    Description = "Reformat nf-core/epitopeprediciton samplesheet file and check its contents."
+    Description = "Reformat nf-core/epitopeprediction samplesheet file and check its contents."
     Epilog = "Example usage: python check_samplesheet.py <FILE_IN> <FILE_OUT>"
 
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
@@ -29,8 +29,8 @@ def print_error(error, context="Line", context_str=""):
 
 def check_allele_nomenclature(allele):
     pattern = re.compile("(^[A-Z][\*][0-9][0-9][:][0-9][0-9])$")
-    if pattern.match(allele):
-        return allele
+    return pattern.match(allele) is not None
+        
 
 def make_dir(path):
     if len(path) > 0:
@@ -45,13 +45,13 @@ def check_samplesheet(file_in, file_out):
     """
     This function checks that the samplesheet follows the following structure:
 
-    SampleID,Alleles,FileName
+    sample,alleles,filename
     GBM_1,A*01:01;A*02:01;B*07:02;B*24:02;C*03:01;C*04:01,gbm_1_anno.vcf|gbm_1_peps.tsv|gbm_1_prot.fasta
     GBM_2,A*02:01;A*24:01;B*07:02;B*08:01;C*04:01;C*07:01,gbm_2_anno.vcf|gbm_2_peps.tsv|gbm_2_prot.fasta
 
     or
 
-    SampleID,Alleles,FileName
+    sample,alleles,filename
     GBM_1,gbm_1_alleles.txt,gbm_1_anno.vcf|gbm_1_peps.tsv|gbm_1_prot.fasta
     GBM_2,gbm_2_alleles.txt,gbm_2_anno.vcf|gbm_2_peps.tsv|gbm_2_prot.fasta
 
@@ -75,14 +75,14 @@ def check_samplesheet(file_in, file_out):
         ## Check header
         COL_NUM = 3
         HEADER = ["sample", "alleles", "filename"]
-        header = [x for x in fin.readline().strip().split(",")]
+        header = [x.strip('"') for x in fin.readline().strip().split(",")]
         if header[: len(HEADER)] != HEADER:
             print("ERROR: Please check samplesheet header -> {} != {}".format("\t".join(header), "\t".join(HEADER)))
             sys.exit(1)
 
         ## Check sample entries
         for line in fin:
-            lspl = [x for x in line.strip().split(",")]
+            lspl = [x.strip('"').replace(" ","") for x in line.strip().split(",")]
             ## Check valid number of columns per row
             if len(lspl) != len(HEADER):
                 print_error(
@@ -100,25 +100,13 @@ def check_samplesheet(file_in, file_out):
         
             sample, alleles, filename = lspl[: len(HEADER)]
 
-            ## Check sample name entries
-            if sample.find(" ") != -1:
-                print_error("Sample entry contains spaces!", "Line", line)
-
-            ## Check if the allele column is filled according to guidelines
-            if alleles.find(" ") != -1:
-                print_error("Alleles entry contains spaces!", "Line", line)
-
-            # Check if the filename column is filled according to guidelines
-            if filename.find(" ") != -1:
-                print_error("Filename entry contains spaces!", "Line", line)
 
             ## Check if the alleles given in the text file are in the right format
             if alleles.endswith(".txt"):
                 with open(alleles, "r") as af:
-                    alleles_from_txt = ""
-                    for al in af.readlines():
-                        alleles_from_txt = alleles_from_txt.strip('\n') + ';' + check_allele_nomenclature(al)
-                alleles = alleles_from_txt.lstrip(';')
+                    alleles = ';'.join([al.strip('\n') if check_allele_nomenclature(al) else \
+                    print_error("Allele format is not matching the nomenclature", "Line", line) for al in af.readlines()])
+                    
 
             ## Get annotation of filename column
             if filename.endswith(".vcf"):
@@ -139,10 +127,10 @@ def check_samplesheet(file_in, file_out):
             if sample not in sample_run_dict:
                 sample_run_dict[sample] = [sample_info]
             else:
-                if sample_info in sample_run_dict[identifier]:
+                if sample_info in sample_run_dict[sample]:
                     print_error("Samplesheet contains duplicate rows!", "Line", line)
                 else:
-                    sample_run_dict[identifier].append(sample_info)
+                    sample_run_dict[sample].append(sample_info)
 
     ## Write validated samplesheet with appropriate columns
     if len(sample_run_dict) > 0:
