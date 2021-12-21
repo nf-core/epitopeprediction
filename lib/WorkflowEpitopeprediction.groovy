f4f457c8e954d006b5c91ae4da8408dfe96585dd
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2021-12-21 12:43:34 +0100

    update worflowepitopeprediction

--- lib/WorkflowEpitopeprediction.groovy
+++ lib/WorkflowEpitopeprediction.groovy
@@ -8,12 +8,7 @@ class WorkflowEpitopeprediction {
     // Check and validate parameters
     //
     public static void initialise(params, log) {
-        //genomeExistsError(params, log)
-
-        //if (!params.fasta) {
-        //    log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-        //    System.exit(1)
-        //}
+        // empty function...
     }
 
     //
@@ -42,18 +37,4 @@ class WorkflowEpitopeprediction {
         yaml_file_text        += "${summary_section}"
         return yaml_file_text
     }
-
-    //
-    // Exit pipeline if incorrect --genome key provided
-    //
-    //private static void genomeExistsError(params, log) {
-    //    if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
-    //        log.error "=============================================================================\n" +
-    //            "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
-    //            "  Currently, the available genome keys are:\n" +
-    //            "  ${params.genomes.keySet().join(", ")}\n" +
-    //            "==================================================================================="
-    //        System.exit(1)
-    //    }
-    //}
 }
