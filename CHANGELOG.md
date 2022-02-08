10f681e094a5a28ac121430ebf42b4d4613f3c23
jonas <jonas-scheid@live.de>
2022-02-08 14:43:33 +0100

    Remove spacing and adjust changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### `Fixed`
 
+- [#136](https://github.com/nf-core/epitopeprediction/pull/135) - Allow variants with unique annotation fields
 ## v2.0.0 - Heuberg - 2021-12-20
 
 ### `Added`
--- bin/epaa.py
+++ bin/epaa.py
@@ -531,7 +531,7 @@ def create_metadata_column_value(pep, c):
     transcript_ids = [x.transcript_id for x in set(pep[0].get_all_transcripts())]
     variants = []
     for t in transcript_ids:
-        variants.extend([v for v in pep[0].get_variants_by_protein(t)]) 
+        variants.extend([v for v in pep[0].get_variants_by_protein(t)])
     meta = set([str(y.get_metadata(c)[0]) for y in set(variants) if len(y.get_metadata(c)) != 0])
     if len(meta) is 0:
         return np.nan
