30ebe24c36ddf4f5eebdcb40eeeea14407c5adc6
Gisela Gabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2023-03-03 10:36:00 +0100

    Update bin/split_peptides.py

--- bin/split_peptides.py
+++ bin/split_peptides.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Written by Sabrina Krakau, Christopher Mohr and released under the MIT license.
+# Written by Sabrina Krakau, Christopher Mohr and released under the MIT license (2022).
 
 
 import math
